361033969c72d2a6ec279b5d94ab3ee5b440274d
Abhishek Choubey <abhishek.choubey@amantyatech.com>
2026-06-23 16:07:00 +0530

    Add a script to maintain first time build related things.

--- ftb.sh
+++ ftb.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
 # ftb: first time build
+#
+# Goal - The goal of this script is to use everything local to build vpp.
+# It expects all dependencies to be present inside the vpp source code.
+#
+# clang-11
+# vpp uses clang-11 by default. It should be packed with this bundle.
+#
 
 ./extras/vagrant/build.sh
